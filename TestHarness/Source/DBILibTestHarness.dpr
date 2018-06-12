029b47c411eb241bbdccb4f4e716bbaf50a27c64
John Vander Reest <jvanderreest@gmail.com>
2018-06-13 09:30:35 +1000

    DBILib - Updated TestHarness dpr to match other test harnesses.

--- TestHarness/Source/DBILibTestHarness.dpr
+++ TestHarness/Source/DBILibTestHarness.dpr
@@ -1,25 +1,40 @@
 program DBILibTestHarness;
 uses
   FastMM4,
+  omFastMMLogging,
+  omFastMMLoggingUtils,
+  MidasLib,
   Forms,
   Windows,
   SysUtils,
-  DB,
-  MidasLib,
-  DBClient,
   omLocals,
   omMVCManagers,
   omApplications,
   omActivitySelfTests,
   DBILibAllUnitTests;
 
+procedure SetupFastMMLoggingFromCmdLine;
+// Need this function defined here because FastMM can only be used in the main dpr.
 begin
-  Application.Initialize;
+  omFastMMLogging.SetFastMMLoggingContext(omFastMMLoggingUtils.GetFastMMLoggingContext(CmdLine, True));
+  FastMM4.DoLeakLogging := omFastMMLogging.GetDoLeakLogging;
+  FastMM4.CustomReportMemoryLeaksOnShutdownProc := omFastMMLogging.AppendEventLog;
+  FastMM4.ShowLeakMessages := omFastMMLogging.GetShowLeakMessage;
+end;
 
+{$R *.RES}
+begin
   SetApplicationClass(TomApplication);
+  TomApplication.LogStartupInfo;
+
+  Application.Initialize;
+  Application.MainFormOnTaskBar := True;
 
   MVCManager := Local(TomMVCManager.Create(nil, 'Dummy -Activity SelfTest')).Obj as TomMVCManager;
 
+  SetupFastMMLoggingFromCmdLine;
+
   Application.Run;
 end.
+
 
